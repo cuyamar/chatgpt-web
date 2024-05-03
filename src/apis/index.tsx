11f98e59fc3f7b760a0c84688111d7e980f12394
sky <sky@cuyar.com>
2024-05-03 19:59:34 +0800

    商品支付页

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,12 +1,11 @@
 import {GptVersion} from "@/app/constants";
 import {useAccessStore} from "@/app/store/access";
 import {MessageRole} from "@/types/chat";
-import {getServerSideConfig} from "@/app/config/server";
 
 // 构建前把localhost修改为你的公网IP或者域名地址
 // const {apiHostUrl} = getServerSideConfig();
 
-const apiHostUrl = "http://c1ea86.r17.cpolar.top";
+const apiHostUrl = "http://localhost:8091";
 
 /**
  * Header 信息
@@ -59,3 +58,29 @@ export const login = (token: string) => {
         body: `code=${accessState.accessCode}`
     });
 };
+
+/**
+ * 商品列表查询
+ */
+export const queryProductList = () => {
+    return fetch(`${apiHostUrl}/api/v1/sale/query_product_list`, {
+        method: "get",
+        headers: getHeaders(),
+    });
+}
+
+/**
+ * 用户商品下单，获得支付地址 url
+ */
+export const createPayOrder = (productId: number) => {
+    return fetch(`${apiHostUrl}/api/v1/sale/create_pay_order`, {
+        method: "post",
+        headers: {
+            ...getHeaders(),
+            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+        },
+        body: `productId=${productId}`
+    });
+}
+
+
